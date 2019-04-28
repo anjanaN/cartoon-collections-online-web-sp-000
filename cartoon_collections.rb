bf14c897377926be8dd65f500170aa6b3ba1ce6a
Anjana Nair <anju740@gmail.com>
2019-04-28 14:13:05 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -17,6 +17,8 @@ def long_planeteer_calls(array)
 end
 
 def find_the_cheese(array)
-  # the array below is here to help
   cheese_types = ["cheddar", "gouda", "camembert"]
+  array.find do |food|
+    cheese_types.include?(food)
+  end
 end
